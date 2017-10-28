129bb363c58ec094c33d8d6c2e46adcb62792847
Abul Basar <abulbasar@users.noreply.github.com>
2017-10-28 23:10:40 +0530

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -1,20 +1,35 @@
 class Batchable:
-
-    def __init__(self, X, y, batch_size = 256, seed = None):
-        import math
-        import numpy as np
-        if seed:
-            np.random.seed(seed)
-        idx = np.arange(X.shape[1])
+    import math
+    import numpy as np
+    
+    def shuffle(self):
+        X = self.X
+        y = self.y
+        idx = np.arange(X.shape[0])
         np.random.shuffle(idx)
-        self.X = X[:, idx]
-        self.y = y[:, idx]
+        self.X = X[idx, :]
+        self.y = y[idx]
+        return
+    
+    def __init__(self, X, y, batch_size = 128, seed = 1):
+        np.random.seed(seed)
+        self.X = X
+        self.y = y
+        self.shuffle()
         self.start = 0
         self.batch_size = batch_size
         self.num_batches = math.ceil(X.shape[0] / batch_size)
     
     def next(self):
+        start = self.start
         end = self.start + self.batch_size
-        if end > self.X.shape[1]:
-            end = self.X.shape[1] - 1
-        return self.X[:, self.start: (end + 1)], self.y[:, self.start: (end + 1)]
+        end = min(self.X.shape[0], end)
+        self.start = end % self.X.shape[0]
+        return self.X[start: end, :], self.y[start: end]
+    
+def show(scores, ax = None):
+    df = pd.DataFrame.from_dict(scores)
+    if ax is None:
+        _, ax = plt.subplots()
+    df.plot.line(alpha = 0.4, ax = ax)
+    df.rolling(100, min_periods=1).mean().plot.line(ax = ax)    
