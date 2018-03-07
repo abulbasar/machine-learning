0efd9ff8d5725655494a9e991073bcb1f8e06032
Abul Basar <abulbasar@users.noreply.github.com>
2018-03-07 23:23:05 +0530

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -112,3 +112,14 @@ class CifarLoader(object):
             row_format.format("labels", str(self.labels))
         ]
         return "\n".join(lines)
+
+   
+   
+import numpy as np
+
+def outliers_iqr(ys):
+    quartile_1, quartile_3 = np.percentile(ys, [25, 75])
+    iqr = quartile_3 - quartile_1
+    lower_bound = quartile_1 - (iqr * 1.5)
+    upper_bound = quartile_3 + (iqr * 1.5)
+    return np.where((ys > upper_bound) | (ys < lower_bound))
