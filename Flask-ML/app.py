5fab08b3fe2ad44c1b9f6eda1d507d1e1f927dcc
Abul Basar <abulbasar@gmail.com>
2019-05-28 21:58:09 +0530

    flask-app for ml serving

---
+++ Flask-ML/app.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+First run train.py. That scripts train a model for insurance price prediction and save the model.
+$ python train.py
+
+The saved model is loaded from the disk. The reloaded model can be used without further training. 
+
+Start the flask application server
+$ python app.py
+
+
+After starting the app server, open this link in the browser http://localhost:5000
+Enter a sample record and submit to receive an estimated price.
+
+"""
+
+
+from flask import Flask, render_template, request
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+# Load the model
+with open("/tmp/model.pickle", "rb") as f:
+    model = pickle.load(f)
+
+# Function to predict single input record
+def predict(record):
+    df_input = pd.DataFrame.from_dict([record])
+    prediction = 10 ** model.predict(df_input)
+    return prediction[0]
+
+
+@app.route('/')
+def home():
+    return render_template('input.html')
+    
+
+@app.route('/predict_price',methods = ["GET", "POST"])
+def result():
+    request_data = None
+    if request.method == "POST":
+        request_data = dict(request.form)
+        print(request_data)
+        prediction = predict(request_data)
+        
+    return render_template("output.html", record = request_data, prediction = prediction)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Flask-ML/train.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import pandas as pd
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+from sklearn import pipeline, preprocessing, compose, linear_model, impute, model_selection
+
+
+# Load data
+df = pd.read_csv("https://raw.githubusercontent.com/abulbasar/data/master/insurance.csv")
+df.head()
+
+target = "charges"
+y = np.log10(df[target])
+X = df.drop(columns=[target])
+
+
+# Categorical features
+cat_columns = ["gender", "smoker", "region"]
+
+# Numeric features
+num_columns = ["age", "bmi", "children"]
+
+
+# Build pipelines for categorical data and numeric data
+cat_pipe = pipeline.Pipeline([
+    ('imputer', impute.SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', preprocessing.OneHotEncoder(handle_unknown='error', drop="first"))
+]) 
+
+num_pipe = pipeline.Pipeline([
+    ('imputer', impute.SimpleImputer(strategy='median')),
+    ('poly', preprocessing.PolynomialFeatures(degree=2, include_bias=False)),
+    ('scaler', preprocessing.StandardScaler()),
+])
+
+preprocessing_pipe = compose.ColumnTransformer([
+    ("cat", cat_pipe, cat_columns),
+    ("num", num_pipe, num_columns)
+])
+
+
+# Build estimator pipeline
+estimator_pipe = pipeline.Pipeline([
+    ("preprocessing", preprocessing_pipe),
+    ("est", linear_model.ElasticNet(random_state=1))
+])
+
+
+# Parameter grid to tune hyper parameters
+param_grid = {
+    "est__alpha": 0.0 + np.random.random(10) * 0.02,
+    "est__l1_ratio": np.linspace(0.0001, 1, 20),
+}
+
+# Grid Search estimator
+gsearch = model_selection.GridSearchCV(estimator_pipe, param_grid, cv = 5, verbose=1, n_jobs=8)
+
+# Fit grid search estimator
+gsearch.fit(X, y)
+
+print(gsearch.best_score_, gsearch.best_params_)
+
+# Sanity test the quality of model
+y_pred = gsearch.predict(X)
+plt.scatter(y, y_pred - y)
+
+pd.DataFrame({"actual": y, "predict": y_pred}).sample(10)
+
+
+# Save the tuned model
+with open("/tmp/model.pickle", "wb") as f:
+    pickle.dump(gsearch, f)
